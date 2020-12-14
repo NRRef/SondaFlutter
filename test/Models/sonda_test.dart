bf1ff3b88f86d32ef05068439dcb17b5a698aafd
NRRef <nyelthon@hotmail.com>
2020-12-13 22:12:10 -0300

    sonda_tests_definition

---
+++ test/Models/sonda_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sonda_flutter/Models/Sonda.dart';
+
+void main() {
+  test('Validation pass', () {});
+  test('Validation fail 1', () {});
+  test('Validation fail 2', () {});
+  test('Validation fail 3', () {});
+  test('Initial properties set', () {});
+  test('Turn two times and stays in W', () {});
+  test('Turn three times and stays in N', () {});
+  test('Move forward', () {});
+  test('Construct and execute a set of instructions', () {});
+}
