58da05f1d3fd9a7ba51c054ad0712e40b3b8ebc5
NRRef <nyelthon@hotmail.com>
2020-12-15 10:02:29 -0300

    basic_layout

--- lib/Views/Home.dart
+++ lib/Views/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sonda_flutter/Models/InputAutomate.dart';
 
 class Home extends StatefulWidget {
   Home({Key key, this.title}) : super(key: key);
@@ -8,52 +9,84 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int _counter = 0;
+  final TextEditingController inputController = new TextEditingController();
+  final TextEditingController outputController = new TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void _moveRovers() {}
+
+  bool _validateInput(String input) {
+    return InputAutomate.validateInput(input);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title), actions: <Widget>[
-        IconButton(
-          icon: const Icon(Icons.refresh),
-          tooltip: 'Show Snackbar',
-          onPressed: () {},
-        ),
-      ]),
-      body: Container(
-        padding: EdgeInsets.all(15),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              height: 200,
-              child:TextField(
-              
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                isCollapsed: true,
-                labelText: 'Entrada',
+        appBar: AppBar(title: Text(widget.title), actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            tooltip: 'Refresh',
+            onPressed: () {},
+          ),
+        ]),
+        body: GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: () {
+            FocusScope.of(context).requestFocus(new FocusNode());
+          },
+          child: SafeArea(
+              child: ListView(
+            children: [
+              Container(
+                padding: EdgeInsets.all(15),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      height: 200,
+                      padding: EdgeInsets.fromLTRB(0, 0, 0, 15),
+                      child: TextField(
+                        controller: inputController,
+                        textAlignVertical: TextAlignVertical.top,
+                        maxLines: null,
+                        minLines: null,
+                        expands: true,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            focusColor: Colors.blue,
+                            labelText: 'Input',
+                            alignLabelWithHint: true),
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      padding: EdgeInsets.fromLTRB(0, 0, 0, 15),
+                      child: ElevatedButton(
+                        onPressed: _moveRovers,
+                        style: ButtonStyle(),
+                        child: Text("Move Rovers"),
+                      ),
+                    ),
+                    Container(
+                      height: 200,
+                      padding: EdgeInsets.fromLTRB(0, 0, 0, 15),
+                      child: TextField(
+                        textAlignVertical: TextAlignVertical.top,
+                        readOnly: true,
+                        maxLines: null,
+                        minLines: null,
+                        expands: true,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            focusColor: Colors.blue,
+                            labelText: 'Output',
+                            alignLabelWithHint: true),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
+            ],
+          )),
+        ));
   }
 }
